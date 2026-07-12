ef0a303674791665cb964d996edac475e50ddaf1
tchindagabriel59-arch <tchindagabriel59@gmail.com>
2026-07-12 21:05:07 +0000

    Update sitemap.ts
    AJOUTE FICHIER MODIFIER

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import { MetadataRoute } from "next";
 
-const SITE_URL = "https://lovelink-omega.vercel.app";
+const SITE_URL = "https://lovelink237.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
