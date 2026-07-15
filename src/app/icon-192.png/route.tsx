b726ee61d3372210f91409d30c33d0cfae2dc61e
tchindagabriel59-arch <tchindagabriel59@gmail.com>
2026-07-15 20:35:01 +0000

    Create route.tsx
    ajoute nouveau fichier

---
+++ src/app/icon-192.png/route.tsx
@@ -0,0 +1,33 @@
+import { ImageResponse } from "next/og";
+
+export const runtime = "edge";
+export const contentType = "image/png";
+export const size = { width: 192, height: 192 };
+
+export async function GET() {
+  return new ImageResponse(
+    (
+      <div
+        style={{
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          background: "white",
+        }}
+      >
+        <img
+          src="https://i.ibb.co/Y4h4H7R7/LOVELINK-1.jpg"
+          alt="LoveLink"
+          width={192}
+          height={192}
+          style={{ objectFit: "cover" }}
+        />
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
