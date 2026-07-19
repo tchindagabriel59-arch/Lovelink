128abe7be1606ef18981fef2aa6859bce4315daf
tchindagabriel59-arch <tchindagabriel59@gmail.com>
2026-07-19 17:55:26 +0000

    Create FacebookPixel.tsx
    ajoute nouveau fichier

---
+++ src/app/components/FacebookPixel.tsx
@@ -0,0 +1,63 @@
+'use client';
+
+import Script from 'next/script';
+import { usePathname, useSearchParams } from 'next/navigation';
+import { useEffect, Suspense } from 'react';
+
+const FB_PIXEL_ID = '1502714508561130';
+
+declare global {
+  interface Window {
+    fbq: (...args: any[]) => void;
+  }
+}
+
+function PixelTracker() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    if (typeof window.fbq === 'function') {
+      window.fbq('track', 'PageView');
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
+
+export default function FacebookPixel() {
+  return (
+    <>
+      <Script
+        id="fb-pixel"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '${FB_PIXEL_ID}');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      <noscript>
+        <img
+          height="1"
+          width="1"
+          style={{ display: 'none' }}
+          src={`https://www.facebook.com/tr?id=${FB_PIXEL_ID}&ev=PageView&noscript=1`}
+          alt=""
+        />
+      </noscript>
+      <Suspense fallback={null}>
+        <PixelTracker />
+      </Suspense>
+    </>
+  );
+}
